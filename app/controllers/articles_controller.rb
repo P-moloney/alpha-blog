b98b34110a1343cd2f74ed005880ae02bd23bd39
P-moloney <moloneypatrick@rocketmail.com>
2023-03-13 13:11:59 +0000

    fixes validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,23 @@
 class ArticlesController < ApplicationController
-
   def show
-    #debugger #pauses code and shows you where in the code
-    @article = Article.find(params[:id]) # @ makes a varaible and instance variable
+    @article = Article.find(params[:id])
   end
-
   def index
     @articles = Article.all
   end
 
   def new
-    @article = Article.new #creates and article on the article/new page loading
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
+
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
     else
-      render 'new'
+      render :new, status: :unprocessable_entity
     end
   end
 
