9b57816b74a4531f539163fe9454af9a63cec6b3
P-moloney <moloneypatrick@rocketmail.com>
2023-03-13 13:04:57 +0000

    adding validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,18 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new #creates and article on the article/new page loading
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
 end
