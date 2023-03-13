76b637059a0ea069095db57d50b034a8c4778503
P-moloney <moloneypatrick@rocketmail.com>
2023-03-13 14:18:09 +0000

    added editing functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,11 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
 
@@ -18,6 +23,16 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice]= "Article was updated successfully"
+      redirect_to @article
+    else
+      render :edit , status: :unprocessable_entity
     end
   end
 
