9a67c70630bd69f9fa2b1b94110422eb3c978207
P-moloney <moloneypatrick@rocketmail.com>
2023-03-15 14:11:59 +0000

    dry code and refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:show, :edit, :update, :destroy] #perform this action before we do the methods
+
   def show
-    @article = Article.find(params[:id])
+
   end
   def index
     @articles = Article.all
@@ -11,12 +13,12 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
 
     if @article.save
       flash[:notice] = "Article was created successfully."
@@ -27,8 +29,8 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+
+    if @article.update(article_params)
       flash[:notice]= "Article was updated successfully"
       redirect_to @article
     else
@@ -37,9 +39,20 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+
     @article.destroy
     redirect_to articles_path
   end
+
+  private #any actions put below are only availbe to this controller
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 
 end
