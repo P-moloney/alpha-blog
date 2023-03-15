75025b3f34d75a580cade90ab3952c74922803c2
P-moloney <moloneypatrick@rocketmail.com>
2023-03-15 12:22:23 +0000

    Adding links to different pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about' #setting up about page
 
-  resources :articles, only: [:show, :index, :new, :create,:edit, :update] #defines what actions it can use
+  resources :articles
 end
